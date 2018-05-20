60ebb17538e59e481f6bedd2ffefdc242983db23
jareks229 <36000024+jareks229@users.noreply.github.com>
2018-05-20 21:51:36 +0200

    Done !

---
+++ Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Data;
+
+namespace Warsztaty_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var listManager = new ProgramLogic();
+            Console.WriteLine("******WITAJ W PROGRAMIE TYPU TASK MANAGER !********");
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("Dostępne polecenia: ");
+            string[] avalibleCommands = new[]
+            {
+                "add -> Dodaj zadanie", "remove -> Usuń zadanie", "show -> Pokaż aktualne zadania", "save -> Zapisz",
+                "load -> Wczytaj (z pliku)",
+                "aval -> Dostępne polecenia"
+            };
+            foreach (var aval in avalibleCommands)
+            {
+                Console.WriteLine(aval);
+            }
+
+            do
+            {
+                string command;
+
+                Console.WriteLine("Wpisz polecenie: ");
+                command = Console.ReadLine();
+
+                if (command == "add")
+                {
+
+                    Console.WriteLine("--- DODAWANIE ZADANIA ---");
+                    Console.WriteLine("*************************");
+
+                    var desc = AskForString("Podaj nazwe zadania");
+
+                    var isImportant = AskForBool("Czy zadanie ma byc oznaczone jako wazne? (T/N): ");
+                    var isAllDayTask = AskForBool("Czy zadanie ma byc calodniowe? (T/N): ");
+
+                    DateTime from;
+                    DateTime? to = null;
+
+                    if (isAllDayTask)
+                    {
+                        from = AskForDate("Data zadania");
+                    }
+                    else
+                    {
+                        from = AskForDate("Data rozpoczecia zadania");
+                        to = AskForDate("Data zakonczenia zadania");
+                    }
+
+                    listManager.AddTask(desc, from, to, isImportant);
+                    
+                }
+
+                if (command == "remove")
+                    {
+                        Console.WriteLine("--- USUWANIE ZADANIA ---");
+                        Console.WriteLine("************************");
+
+                        if (listManager.TaskCount == 0)
+                        {
+                            Console.WriteLine("Brak zadan!");
+                            break;
+                        }
+
+                        int numberOfTask = AskForNumberOfTask("Podaj nr zadania", listManager.TaskCount);
+
+                        listManager.RemoveTask(numberOfTask);
+                        
+
+                    }
+
+                if (command == "show")
+                {
+                    listManager.ShowTasks();
+                    
+                }
+
+                if (command == "save")
+                {
+                    listManager.SaveTasks();
+                    
+                }
+
+                if (command == "load")
+                {
+                    listManager.LoadTasks();
+                    
+
+                }
+
+                if (command == "aval")
+                {
+                    foreach (var avalible in avalibleCommands)
+                    {
+                        Console.WriteLine(avalible);
+                    }
+
+                    
+                }
+
+            } while (true);
+
+        }
+
+        private static DateTime AskForDate(string dateName)
+        {
+            Console.Write("{0} (rrrr-mm-dd): ", dateName);
+            var dateString = Console.ReadLine();
+            var dateStringParseOk = DateTime.TryParse(dateString, out var date);
+            if (!dateStringParseOk)
+            {
+                Console.WriteLine($"Podana data {dateString} jest nieprawidlowa. Sprobuj jeszcze raz stosujac format rrrr-mm-dd!");
+                AskForDate(dateName);
+            }
+
+            return date;
+        }
+
+        //Podpatrzone u Rafała jak robił:
+        private static bool AskForBool(string question)
+        {
+            Console.Write($"{question} (T/N):");
+            var answer = Console.ReadLine().ToLower();
+
+            if (answer != "t" && answer != "n")
+            {
+                Console.WriteLine("Odpowiadaj tylko 'T' lub 'N'!");
+                AskForBool(question);
+            }
+
+            return answer == "t";
+        }
+
+        private static string AskForString(string question)
+        {
+            Console.Write($"{question}: ");
+            var result = Console.ReadLine();
+
+            return result;
+        }
+
+        private static int AskForNumberOfTask(string question, int count)
+        {
+            var input = AskForString(question);
+            var parsedSuccesfully = int.TryParse(input, out int i);
+            if (!parsedSuccesfully)
+            {
+                Console.WriteLine("Nie podałeś liczby !");
+                i = AskForNumberOfTask(question, count);
+            }
+
+            if (i < 0 || i > count)
+            {
+                Console.WriteLine($"Podałeś {count}, a lista zawiera elementy 1-{1}");
+                i = AskForNumberOfTask(question, count);
+            }
+
+            return i;
+        }
+    }
+    }
+
+    public class TaskModel
+    {
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public bool IsImportant { get; set; }
+
+        public bool AllDay { get; set; }
+
+        public TaskModel(string description, DateTime from, DateTime? to, bool isImportant)
+        {
+            Description = description;
+            StartDate = from;
+            EndDate = to;
+            IsImportant = isImportant;
+
+            if (to.HasValue)
+            {
+                AllDay = true;
+            }
+        }
+    }
+
---
+++ ProgramLogic.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Warsztaty_1
+{
+    public class ProgramLogic
+    {
+        private List<TaskModel> TaskModelList = new List<TaskModel>();
+
+        private const string _path = @"data.csv";
+
+        public int TaskCount
+        {
+            get
+            {
+                return TaskModelList.Count;
+            }
+        }
+
+        public void AddTask(string description, DateTime from, DateTime? to, bool isImportant)
+        {
+            var task = new TaskModel(description, from, to, isImportant);
+
+            TaskModelList.Add(task);
+        }
+
+        public void RemoveTask(int taskNumber)
+        {
+            int index = taskNumber - 1;
+
+            //wtedy nie mieliśmy lambdy ale dodałem gdy wszedł moduł zaawansowany C#
+            TaskModelList.Sort((x, y) => { return x.StartDate.CompareTo(y.StartDate); });
+
+            TaskModelList.RemoveAt(index);
+        }
+
+        public void ShowTasks()
+        {
+            Console.WriteLine("------------------------------ LISTA ZADAN -------------------------------");
+
+            TaskModelList.Sort((x, y) => { return x.StartDate.CompareTo(y.StartDate); });
+
+            if (TaskModelList.Count == 0)
+            {
+                Console.WriteLine("Brak zadan do wykonania!");
+                return;
+            }
+
+            Console.WriteLine("| {0} | {1} | {2} | {3} | {4} |",
+                    "#".PadLeft(4),
+                    "Nazwa".PadRight(30),
+                    "Data od".PadRight(10),
+                    "Data do".PadRight(10),
+                    "Priorytet"
+                    );
+
+            for (var i = 0; i < TaskModelList.Count; i++)
+            {
+                var tm = TaskModelList[i];
+
+                var desc = tm.Description;
+                if (desc.Length > 28) desc = $"{desc.Substring(0, 25)}...";
+
+                Console.WriteLine("| {0} | {1} | {2} | {3} | {4} |",
+                    (i + 1).ToString().PadLeft(4),
+                    desc.PadRight(30),
+                    tm.StartDate.ToString("yyyy-MM-dd"),
+                    tm.EndDate.HasValue ? tm.EndDate.Value.ToString("yyyy-MM-dd") : "N/A".PadRight(10),
+                    tm.IsImportant ? "(!)".PadRight(9) : "".PadRight(9)
+                    );
+            }
+
+        }
+
+        public void SaveTasks()
+        {
+            TaskModelList.Sort((x, y) => { return x.StartDate.CompareTo(y.StartDate); });
+
+            if (TaskModelList.Count == 0)
+            {
+                Console.WriteLine("Brak zadan do zapisania!");
+                return;
+            }
+
+            using (var writer = new StreamWriter(Path.GetFullPath(_path), false))
+            {
+                foreach (var task in TaskModelList)
+                {
+                    writer.WriteLine("{0},{1},{2},{3},{4}",
+                        task.Description,
+                        task.StartDate.ToString("yyyy-MM-dd"),
+                        task.EndDate.HasValue ? task.EndDate.Value.ToString("yyyy-MM-dd") : "",
+                        task.IsImportant ? "T" : "N",
+                        task.AllDay ? "T" : "N"
+                    );
+                }
+            }
+
+            Console.WriteLine("Zapisano do pliku!");
+        }
+
+        public void LoadTasks()
+        {
+            if (File.Exists(_path))
+            {
+                Console.WriteLine("(!) Nadpisujesz obecne zadania !");
+            }
+
+            int recordLoaded = 0;
+
+            using (var reader = new StreamReader(_path))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var record = line.Split(',');
+
+                    if (record.Length != 5)
+                    {
+                        Console.WriteLine("(!) Zły plik ! Spróbuj inny !");
+                        TaskModelList.Clear();
+                        break;
+                    }
+
+                    bool fromParsed = DateTime.TryParse(record[1], out var from);
+                    if (fromParsed == false)
+                    {
+                        Console.WriteLine($"(!) Błąd daty ! (Data od) !");
+                        Console.WriteLine($"POMIJAM: {record}");
+                        continue;
+                    }
+
+                    DateTime? to = null;
+                    if (record[2] != string.Empty)
+                    {
+                        bool toParsed = DateTime.TryParse(record[2], out var toNullable);
+                        if (fromParsed == false)
+                        {
+                            Console.WriteLine("(!) Blad struktury rekordu (Data od) !");
+                            Console.WriteLine( "POMIJAM: {0}", record);
+                            continue;
+                        }
+
+                        to = toNullable;
+                    }
+
+                    bool isImportant = record[3] == "T" ? true : false;
+                    bool isAllDay = record[4] == "T" ? true : false;
+
+                    var task = new TaskModel(record[0], from, to, isImportant);
+
+                    TaskModelList.Add(task);
+                    recordLoaded++;
+                }
+            }
+
+            Console.WriteLine($"Wczytano {recordLoaded} zadań");
+            Console.WriteLine($"W pamieci jest {TaskCount} zadań");
+        }
+    }
+}
